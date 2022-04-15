638758b43092121d94a9c19ed867e450a1a491da
Prog'z <39605375+ThomasProg@users.noreply.github.com>
2022-04-15 08:16:50 +0200

    Utility System added to the project

---
+++ Assets/StrategyAI/StrategyAI.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UtilitySystem;
+
+public class StrategyAI : MonoBehaviour
+{
+    UtilitySystem.UtilitySystem objectif;
+    UtilitySystem.UtilitySystem subjectif;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
