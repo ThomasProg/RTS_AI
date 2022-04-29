f83a7a256126e80c7cef3ae0c9dd027937796f86
Prog'z <39605375+ThomasProg@users.noreply.github.com>
2022-04-29 16:01:19 +0200

    setting modified

--- Assets/Scripts/AI/Strategy/Tasks/Capture/SquadCapturePoint.cs
+++ Assets/Scripts/AI/Strategy/Tasks/Capture/SquadCapturePoint.cs
@@ -17,7 +17,7 @@ public class SquadCapturePoint : InBetweenTask
 
     float GetCapturePointTacticalScore(TargetBuilding targetBuilding, Vector2 pos)
     {
-        return -Vector2.Distance(targetBuilding.Get2DPosition(), pos) / 30f;
+        return -Vector2.Distance(targetBuilding.Get2DPosition(), pos) / 500f;
     }
 
     IEnumerator StartAsync()
