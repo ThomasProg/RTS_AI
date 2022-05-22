96703b9a57433d67af44c0e4d03dad417a9f8c55
Six Jonathan <Six-jonathan@orange.fr>
2022-05-22 22:59:05 +0200

    Fix (or not) capture point by unit

--- Assets/Scripts/Entities/TargetBuilding.cs
+++ Assets/Scripts/Entities/TargetBuilding.cs
@@ -69,7 +69,6 @@ public class TargetBuilding : MonoBehaviour, IInfluencer
 
         if (CapturingTeam == ETeam.Neutral)
         {
-            if (TeamScore[(int)GameServices.GetOpponent(unit.GetTeam())] == 0)
             {
                 CapturingTeam = unit.GetTeam();
                 GaugeImage.color = GameServices.GetTeamColor(CapturingTeam);
