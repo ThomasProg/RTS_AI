0208d3a0c6a2a093731276b62806a91ac503f7f8
Six Jonathan <Six-jonathan@orange.fr>
2022-05-22 23:03:28 +0200

    Second iteration of factory score

--- Assets/Scripts/AI/Strategy/PointOfInterest/FactoryPoI.cs
+++ Assets/Scripts/AI/Strategy/PointOfInterest/FactoryPoI.cs
@@ -40,19 +40,17 @@ public class FactoryPoI : PointOfInterest
         // - Apply coefficient to this result depending on distance from our base or enemy base. Depending on AI personality
         // - Apply direct coefficient depending on AI personality
         
-        // If ai target a point that it don't own, evaluate priority to defend this point
         if (stratAI.controller.Team != factory.GetTeam())
         {
-            EvaluateTakeThisPointPriority(aiSquads, playerController);
+            EvaluateTakeThisPointPriority(aiSquads);
         }
-        // If player can attack a near target without and can win it, evaluate priority to attack this point
         else if (stratAI.controller.Team == factory.GetTeam())
         {
-            EvaluatedefendThisPointPriority(aiSquads, playerSquads, playerController);
+            EvaluateDefendThisPointPriority(aiSquads);
         }
     }
     
-    void EvaluateTakeThisPointPriority(IEnumerable aiSquads, PlayerController playerController)
+    void EvaluateTakeThisPointPriority(IEnumerable aiSquads)
     {
         // Get all enemy squads should attack this point
         // Accept enemy squad only if probability is upper than X % (based on 50% +/- AI personality)
@@ -85,34 +83,49 @@ public class FactoryPoI : PointOfInterest
         else
         {
             priority = 6f;
+            priority += playerStrength == 0 ? 1 : aiStrength / playerStrength;
         }
         
         // Apply direct coefficient depending on AI personality
         // TODO:
     }
 
-    void EvaluatedefendThisPointPriority(IEnumerable enemySquad, IEnumerable playerSquads, PlayerController playerController)
-    {
-        bool squadFound = false;
-        foreach (Squad squad in playerSquads)
-        {
-            // TODO: Check if squad is occuped with highter priority task
-            
-            float squadToTargetSqrDist = (factory.GetInfluencePosition() - squad.GetAveragePosition()).sqrMagnitude;
-            float playerSquadStrength = Statistic.EvaluateSquadsStrengthInZone(new []{squad}, factory.GetInfluencePosition(), squadToTargetSqrDist);
-            float aiStrength = Statistic.EvaluateSquadsStrengthInZone(enemySquad, factory.GetInfluencePosition(), squadToTargetSqrDist);
 
-            if (playerSquadStrength > aiStrength)
+    void EvaluateDefendThisPointPriority(IEnumerable aiSquads)
+    {
+        // Get all enemy squads should attack this point
+        // Accept enemy squad only if probability is upper than X % (based on 50% +/- AI personality)
+        List<Statistic.POITargetByEnemySquad> playerSquadObjectives = 
+            Statistic.GetPOITargetByEnemySquad(this, GameServices.GetPlayerController().GetTeam() , 10f, 1.1f, 0.5f);
+
+        float distPlayerUnitsToTarget = float.MinValue;
+        float playerStrength = 0f;
+        foreach (Statistic.POITargetByEnemySquad playerSquadObjective in playerSquadObjectives)
+        {
+            float sqrDistSquadTarget =
+                (playerSquadObjective.enemy.GetAveragePosition() - playerSquadObjective.poi.position)
+                .sqrMagnitude;
+            if (distPlayerUnitsToTarget < sqrDistSquadTarget)
             {
-                priority = 6f;
-                squadFound = true;
-                break;
+                distPlayerUnitsToTarget = sqrDistSquadTarget;
             }
+            
+            playerStrength += playerSquadObjective.enemy.GetStrength();
         }
+
+        float aiStrength =
+            Statistic.EvaluateSquadsStrengthInZone(aiSquads, factory.GetInfluencePosition(), distPlayerUnitsToTarget);
         
-        if (!squadFound)
+        // Process the balance of power and evaluate the cost of loose/keep this point. Depending on AI personality
+        if (playerStrength > aiStrength)
+        {
+            priority = 6f;
+        }
+        else
+        {
             priority = 0f;
-        
+        }
+
         // Apply direct coefficient depending on AI personality
         // TODO:
     }
